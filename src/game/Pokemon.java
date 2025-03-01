67f23efa0cdddf9434a55dc351a3a79f7cf81fde
Spark <123284870+MadManJJ@users.noreply.github.com>
2025-03-01 20:16:08 +0700

    Add filepath to Pokemon.java

--- src/game/Pokemon.java
+++ src/game/Pokemon.java
@@ -2,125 +2,128 @@ package game;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList ;
+import java.util.ArrayList;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken; 
-
+import com.google.gson.reflect.TypeToken;
 
 public class Pokemon {
 	// general info
-	private String name ; 
-	private int pokemonId ; 
-	private ArrayList<PokemonType> pokemonTypes ; 
-	
+	private String name;
+	private int pokemonId;
+	private ArrayList<PokemonType> pokemonTypes;
+	private String filePath;
+
 	// basic stats
-	private int hp ; // current hp
-	private int max_hp ; 
-	private int attack ; 
-	private int defense ; 
-	private int specialAttack ; 
-	private int specialDefense ; 
-	private int speed ; 
-	
+	private int hp; // current hp
+	private int max_hp;
+	private int attack;
+	private int defense;
+	private int specialAttack;
+	private int specialDefense;
+	private int speed;
+
 	// moves
-	private ArrayList<Move> moves ; 
-	
+	private ArrayList<Move> moves;
+
 	// status
-	private Status status ; 
-	
+	private Status status;
+
 	// static list to hold all available pokemon
 	private static ArrayList<Pokemon> pokemonList = new ArrayList<>();
-	
+
 	// to create new pokemon
 	public Pokemon(int pokemonId) {
-	    // Loop through the pokemonList to find the pokemon with the given pokemonId
-	    for (Pokemon existingPokemon : pokemonList) {
-	        if (existingPokemon.getPokemonId() == pokemonId) {
-	            // Set this object's properties from the existingPokemon
-	            this.name = existingPokemon.name;
-	            this.pokemonId = existingPokemon.pokemonId;
-	            this.pokemonTypes = new ArrayList<>(existingPokemon.pokemonTypes); // Deep copy of list
-	            this.max_hp = existingPokemon.max_hp;
-	            this.hp = existingPokemon.hp;  // Set initial HP (could be full or other logic)
-	            this.attack = existingPokemon.attack;
-	            this.defense = existingPokemon.defense;
-	            this.specialAttack = existingPokemon.specialAttack;
-	            this.specialDefense = existingPokemon.specialDefense;
-	            this.speed = existingPokemon.speed;
-	            this.moves = new ArrayList<>(existingPokemon.moves);  // Deep copy of list
-	            this.status = existingPokemon.status;
+		// Loop through the pokemonList to find the pokemon with the given pokemonId
+		for (Pokemon existingPokemon : pokemonList) {
+			if (existingPokemon.getPokemonId() == pokemonId) {
+				// Set this object's properties from the existingPokemon
+				this.name = existingPokemon.name;
+				this.pokemonId = existingPokemon.pokemonId;
+				this.filePath = existingPokemon.filePath;
+				this.pokemonTypes = new ArrayList<>(existingPokemon.pokemonTypes); // Deep copy of list
+				this.max_hp = existingPokemon.max_hp;
+				this.hp = existingPokemon.hp; // Set initial HP (could be full or other logic)
+				this.attack = existingPokemon.attack;
+				this.defense = existingPokemon.defense;
+				this.specialAttack = existingPokemon.specialAttack;
+				this.specialDefense = existingPokemon.specialDefense;
+				this.speed = existingPokemon.speed;
+				this.moves = new ArrayList<>(existingPokemon.moves); // Deep copy of list
+				this.status = existingPokemon.status;
 
-	            // You can add additional logic if you want the new Pokémon to have any modifications
-	            break;  // Once the Pokémon is found and set, exit the loop
-	        }
-	    }
+				// You can add additional logic if you want the new Pokémon to have any
+				// modifications
+				break; // Once the Pokémon is found and set, exit the loop
+			}
+		}
 	}
 
 	public boolean isDead() {
 		return (this.hp == 0);
 	}
-	
+
 	public static void listPokemons() {
-	    // Loop through all the Pokémon in the pokemonList
+		// Loop through all the Pokémon in the pokemonList
 		int i = 0;
-	    for (Pokemon p : pokemonList) {
-	        // Print the Pokémon name (or any other info you want to display)
-	        System.out.println("Name: " + p.name);
-	        System.out.println("ID: " + p.pokemonId);
-	        System.out.println("Types: " + p.pokemonTypes);
-	        System.out.println("HP: " + p.max_hp);
-	        System.out.println("Attack: " + p.attack);
-	        System.out.println("Defense: " + p.defense);
-	        System.out.println("Special Attack: " + p.specialAttack);
-	        System.out.println("Special Defense: " + p.specialDefense);
-	        System.out.println("Speed: " + p.speed);
-	        System.out.println("Status " + p.getStatus());
-	        System.out.println("Moves: ");
-	        for (Move m : p.moves) {
-	            System.out.println("  - " + m.getName() + " (Type: " + m.getPokemonType() + ", Damage: " + m.getDamage() + ")");
-	        }
-	        System.out.println("-----------------------------------------");
-	        i++;
-	        if(i == 5) {
-	        	break;
-	        }
-	    }
+		for (Pokemon p : pokemonList) {
+			// Print the Pokémon name (or any other info you want to display)
+			System.out.println("Name: " + p.name);
+			System.out.println("ID: " + p.pokemonId);
+			System.out.println("FilePath: " + p.filePath);
+			System.out.println("Types: " + p.pokemonTypes);
+			System.out.println("HP: " + p.max_hp);
+			System.out.println("Attack: " + p.attack);
+			System.out.println("Defense: " + p.defense);
+			System.out.println("Special Attack: " + p.specialAttack);
+			System.out.println("Special Defense: " + p.specialDefense);
+			System.out.println("Speed: " + p.speed);
+			System.out.println("Status " + p.getStatus());
+			System.out.println("Moves: ");
+			for (Move m : p.moves) {
+				System.out.println(
+						"  - " + m.getName() + " (Type: " + m.getPokemonType() + ", Damage: " + m.getDamage() + ")");
+			}
+			System.out.println("-----------------------------------------");
+			i++;
+			if (i == 5) {
+				break;
+			}
+		}
 	}
-	
-	
 
 	@Override
 	public String toString() {
-		return "Pokemon [name=" + name + ", pokemonId=" + pokemonId + ", pokemonTypes=" + pokemonTypes + ", hp=" + hp
-				+ ", max_hp=" + max_hp + ", attack=" + attack + ", defense=" + defense + ", specialAttack="
-				+ specialAttack + ", specialDefense=" + specialDefense + ", speed=" + speed + ", moves=" + moves
-				+ ", status=" + status + "]";
+		return "Pokemon [name=" + name + ", pokemonId=" + pokemonId + ", pokemonTypes=" + pokemonTypes + ", filePath="
+				+ filePath + ", hp=" + hp + ", max_hp=" + max_hp + ", attack=" + attack + ", defense=" + defense
+				+ ", specialAttack=" + specialAttack + ", specialDefense=" + specialDefense + ", speed=" + speed
+				+ ", moves=" + moves + ", status=" + status + "]";
 	}
-
 
 	public static void loadPokemonsFromJson(String filePath) {
 		// TODO :
 		// Load pokemon from json file
-	    // Create a new Gson instance
-	    Gson gson = new Gson();
-	    
-	    // Define the type of data to parse (ArrayList of Pokemon objects)
-	    try (FileReader reader = new FileReader(filePath)) {
-	        // Deserialize the JSON into a list of Pokemon objects
-	        ArrayList<Pokemon> pokemons = gson.fromJson(reader, new TypeToken<ArrayList<Pokemon>>() {}.getType());
+		// Create a new Gson instance
+		Gson gson = new Gson();
 
-	        // Populate the pokemonList with the deserialized Pokemon objects
-	        pokemonList.addAll(pokemons);
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
+		// Define the type of data to parse (ArrayList of Pokemon objects)
+		try (FileReader reader = new FileReader(filePath)) {
+			// Deserialize the JSON into a list of Pokemon objects
+			ArrayList<Pokemon> pokemons = gson.fromJson(reader, new TypeToken<ArrayList<Pokemon>>() {
+			}.getType());
+
+			// Populate the pokemonList with the deserialized Pokemon objects
+			pokemonList.addAll(pokemons);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	// getters & setters
 	public String getName() {
 		return name;
 	}
+
 	public int getPokemonId() {
 		return pokemonId;
 	}
@@ -184,9 +187,11 @@ public class Pokemon {
 	public ArrayList<Move> getMoves() {
 		return moves;
 	}
+
 	public void setMoves(ArrayList<Move> moves) {
 		this.moves = moves;
 	}
+
 	public void addMove(Move move) {
 		this.moves.add(move);
 	}
